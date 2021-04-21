05735fd24146c8891078b44eedb7c47684487ace
nicholas <nickwtayl@gmail.com>
2021-04-21 18:58:33 -0400

    make that final right-hand bottom row key into numlock

--- keyboards/afternoonlabs/breeze/keymaps/nichtayl/keymap.c
+++ keyboards/afternoonlabs/breeze/keymaps/nichtayl/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_LEFT, KC_DOWN, KC_RIGHT,
   //└────────┴────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┴────────┴────────┴────────┘└────────┴────────┴────────┘
-                         C_S_T(KC_CAPS), KC_LGUI, KC_LALT, LOWER,            KC_SPC,  RAISE,   KC_LGUI, XXXXXXX
+                         C_S_T(KC_CAPS), KC_LGUI, KC_LALT, LOWER,            KC_SPC,  RAISE,   KC_LGUI, KC_NLCK
   //                           └────────┴────────┴────────┴────────┘        └────────┴────────┴────────┴────────┘
   ),
 
